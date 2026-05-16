a3a6a29b8971c0052ad6097c9fd08f5fab57238b
31elnomad <103994092+31elnomad@users.noreply.github.com>
2026-05-16 18:36:08 +0200

    Retrieve CLIENT_ID and REFRESH_TOKEN from environment
    Added environment variable retrieval for client ID and refresh token.

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from datetime import datetime
 
 print("Le script fonctionne !")
 print("Heure actuelle :", datetime.now())
+
+import os
+
+client_id = os.environ["CLIENT_ID"]
+refresh_token = os.environ["REFRESH_TOKEN"]
+
+print(client_id, refresh_token)
