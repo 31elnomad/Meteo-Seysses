18f2b9e0e4f6c877dee98d39f4790aea580f7319
31elnomad <103994092+31elnomad@users.noreply.github.com>
2026-05-16 18:40:23 +0200

    Add confirmation print statement in main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ print("Le script fonctionne !")
 print("Heure actuelle :", datetime.now())
 client_id = os.environ["CLIENT_ID"]
 refresh_token = os.environ["REFRESH_TOKEN"]
-
 print(client_id, refresh_token)
+print('ok')
