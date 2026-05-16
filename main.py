5ad459eb7ffe7f5ed2c2b2da78c6bbe9dcdf234f
31elnomad <103994092+31elnomad@users.noreply.github.com>
2026-05-16 18:49:50 +0200

    Change print statement to show lengths of tokens

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ print("Le script fonctionne !")
 print("Heure actuelle :", datetime.now())
 client_id = os.environ["CLIENT_ID"]
 refresh_token = os.environ["REFRESH_TOKEN"]
-print(bool(client_id), bool(refresh_token))
+print(len(client_id), len(refresh_token))
 print('ok')
