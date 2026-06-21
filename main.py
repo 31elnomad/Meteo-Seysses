4f9c7e542fdfed62d6f73c33452a44783393092a
31elnomad <103994092+31elnomad@users.noreply.github.com>
2026-06-21 18:04:30 +0200

    Add client_secret length to output
    Fixed typo in variable name and updated print statement.

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from scripts.getdata import Token
 if __name__ == "__main__":
     client_id = os.environ["CLIENT_ID"]
     refresh_token = os.environ["REFRESH_TOKEN"]
+    client_secret = os.environ["CLIENT_SECRET"]
     print("Le script fonctionne !")
     print("Heure actuelle :", datetime.now())
-    print(len(client_id), len(refresh_token))
+    print(len(client_id), len(refresh_token), len(clien_secret))
     print('ok')
