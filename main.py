9c7c9f5a6cce0e35207f15366925c7a6a1b4f646
31elnomad <103994092+31elnomad@users.noreply.github.com>
2026-06-21 17:51:36 +0200

    Refactor main.py to include main guard

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import os
 from scripts.getdata import Token
 
-print("Le script fonctionne !")
-print("Heure actuelle :", datetime.now())
-client_id = os.environ["CLIENT_ID"]
-refresh_token = os.environ["REFRESH_TOKEN"]
-print(len(client_id), len(refresh_token))
-print('ok')
+
+if __name__ == "__main__":
+    client_id = os.environ["CLIENT_ID"]
+    refresh_token = os.environ["REFRESH_TOKEN"]
+    print("Le script fonctionne !")
+    print("Heure actuelle :", datetime.now())
+    print(len(client_id), len(refresh_token))
+    print('ok')
