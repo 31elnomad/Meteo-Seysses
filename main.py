295e35cea4cafe27ab511323f8ab6da2583bb975
31elnomad <103994092+31elnomad@users.noreply.github.com>
2026-06-22 19:04:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from datetime import datetime
 import os
+import time
+import json
 from scripts.getdata_ import get_access_token, get_station_data
 
 a = get_access_token()
 b = get_station_data()
 
-print(b['body']['devices'][0])
-print(b['body']['devices'][0].keys())
+tmp = b['body']['devices'][0]
+
+dashboard = station["dashboard_data"]
+
+results = {
+  "date": time.strftime("%d.%m.%Y à %H:%M"),
+  "pression": dashboard.get("Pressure")
+}
+
+
+
+print(results)
+#print(b['body']['devices'][0].keys())
 
